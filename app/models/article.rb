194df60be881f15c6137373348ead797dcd59b59
Maureenkamau03 <mkaymaureen@gmail.com>
2022-12-06 12:34:04 +0300

    DEV-21 validated models, added seed data

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,9 @@
 class Article < ApplicationRecord
   belongs_to :dev
+
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :minutes_to_read, presence: true
+  validates :featured_image, presence: true
+ 
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,4 +1,9 @@
 class Dev < ApplicationRecord
   has_many :articles
   has_one :profile
+
+  validates :username, presence: true, uniqueness: true
+  validates :lname, presence: true
+  validates :fname, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
